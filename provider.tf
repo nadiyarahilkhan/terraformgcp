34a1bdad11124361aece3b9928941d2ceee924f6
testlab <testmail@ow.com>
2024-12-14 12:39:46 +0530

    updating the file

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
 provider "google" {
   project     = "apt-memento-442605-f0"
   region      = "us-central1"
-  credentials= file("/root/tf_base/terraformgcp/apt-memento-442605-f0-df6dc9fbfdef.json")
+  credentials= file("/root/tf_base/terraformgcp/apt-memento-442605-f0-0c8ea3a0fc68.json")
+}
+resource "google_storage_bucket" "tf" {
+  name     = "tf-bucket-gcp"
+  location = "us-central1"
 }
